b8f47b60ec6bf9cf367874619221d283c0961e3f
EXISTINGG <1906054192@qq.com>
2023-10-11 15:42:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-alert :title="`服务器地址：${url}`" type="success" :closable="false" />
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="name" label="姓名" width="180" />
+    <el-table-column prop="age" label="年龄" width="180" />
+    <el-table-column prop="gender" label="性别" />
+  </el-table>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const tableData = ref([])
+const url = ref('https://koaapi.hxq-001.top')
+
+const initData = async () => {
+  const { data } = await axios.get(url.value);
+
+  tableData.value = data.data;
+}
+
+onMounted(() => initData())
+</script>
